080d229cea097744dff443bb8b035b65d29b6df8
IvanZuzichev <isip_i.s.zuzichev@mpt.ru>
2026-05-30 01:22:38 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ETog/',   
   plugins: [react()],
   build: {
     // Оптимизации для PWA
